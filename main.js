618b4192994cf8e8a65775adde6065bfcb4cfb23
DalvayayUSM <111554532+DalvayayUSM@users.noreply.github.com>
2022-10-26 19:30:10 -0300

    Implementación de botón expandir y editar

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var id = 0;
 const tabla = document.getElementById("tableBodyRecetas");
 const formulario = document.getElementById("formulario");
+const formulario2 = document.getElementById("formulario2");
 let arrayRecetas = [];
 
 $(document).ready(function(){
     var tablaRecetas = $("#dataTableRecetas").DataTable({
         "autoWidth": true,
         "columns": [
-            {"data":"id"},
+            {"data":"id", visible: false},
             {"data":"nombre"},
             {"data":"ingredientes"},
             {"data":"instrucciones"},
-            {"data":"acciones", "render": function(){
+            {
+                "data": "acciones", "width": "12%", "render": function(){
                 return `
-                    <button class="btn btn-info" id="btnInfo" data-bs-toggle="modal" data-bs-target="#agregarReceta"><i class="bi bi-arrow-up-right-square-fill"></i></button>
-                    <button class="btn btn-warning" id="btnEditar" data-bs-toggle="modal" data-bs-target="#agregarReceta"><i class="bi bi-pencil-fill"></i></button>
+                    <button class="btn btn-info" id="btnInfo" ><i class="bi bi-arrow-up-right-square-fill"></i></button>
+                    <button class="btn btn-warning" id="btnEditar"><i class="bi bi-pencil-fill"></i></button>
                     <button type="button" class="btn btn-danger" id="btnBorrar"><i class="bi bi-trash-fill"></i></button>`
             }}
         ]
@@ -25,14 +27,8 @@ $(document).ready(function(){
         var nombre = tablaRecetas.row(row).data().nombre;
         var ingredientes = tablaRecetas.row(row).data().ingredientes;
         var instrucciones = tablaRecetas.row(row).data().instrucciones;
-        
-        $("#nombreReceta").val(nombre);
-        $("#ingredientes").val(ingredientes);
-        $("#instrucciones").val(instrucciones);
-
-        $("#nombreReceta").prop("readonly", true);
-        $("#ingredientes").prop("readonly", true);
-        $("#instrucciones").prop("readonly", true);
+        var info = "Receta: "+nombre+"\n"+"Ingredientes: "+ ingredientes+"\n"+"Instrucciones: "+instrucciones;
+        alert(info);
     });
 
     $("#dataTableRecetas tbody").on("click", "#btnEditar", function () {
@@ -42,13 +38,66 @@ $(document).ready(function(){
         var nombre = tablaRecetas.row(row).data().nombre;
         var ingredientes = tablaRecetas.row(row).data().ingredientes;
         var instrucciones = tablaRecetas.row(row).data().instrucciones;
-        $("#nombreReceta").prop("readonly", false);
-        $("#ingredientes").prop("readonly", false);
-        $("#instrucciones").prop("readonly", false);
-
-        $("#nombreReceta").val(nombre);
-        $("#ingredientes").val(ingredientes);
-        $("#instrucciones").val(instrucciones);
+        let nombreReceta2 = prompt("Cambiar nombre");
+        let ingredientes2 = prompt("Cambiar ingredientes");
+        let instrucciones2 = prompt("Cambiar instrucciones");
+        if (instrucciones2==null||instrucciones2==""){
+            for (let i = 0; i < arrayRecetas.length; i++){
+                if (Object.values(arrayRecetas[i])[2] == instrucciones){
+                    var recetita = arrayRecetas[i];
+                    recetita.instrucciones = instrucciones;
+                }
+            }
+        }
+        else{
+            if (instrucciones!=instrucciones2){
+                for (let i = 0; i < arrayRecetas.length; i++){
+                    if (Object.values(arrayRecetas[i])[2] == instrucciones){
+                        var recetita = arrayRecetas[i];
+                        recetita.instrucciones = instrucciones2;
+                    }
+                }
+            }
+        }
+        if (ingredientes2==null||ingredientes2==""){
+            for (let i = 0; i < arrayRecetas.length; i++){
+                if (Object.values(arrayRecetas[i])[2] == instrucciones){
+                    var recetita = arrayRecetas[i];
+                    recetita.instrucciones = instrucciones;
+                }
+            }
+        }
+        else{
+            if (ingredientes!=ingredientes2){
+                for (let i = 0; i < arrayRecetas.length; i++){
+                    if (Object.values(arrayRecetas[i])[1] == ingredientes){
+                        var recetita = arrayRecetas[i];
+                        recetita.ingredientes = ingredientes2;
+                    }
+                }
+            }
+        }
+        if (nombreReceta2==null||nombreReceta2==""){
+            for (let i = 0; i < arrayRecetas.length; i++){
+                if (Object.values(arrayRecetas[i])[2] == instrucciones){
+                    var recetita = arrayRecetas[i];
+                    recetita.instrucciones = instrucciones;
+                }
+            }
+        }
+        else{
+            if (nombre!=nombreReceta2){
+                for (let i = 0; i < arrayRecetas.length; i++){
+                    if (Object.values(arrayRecetas[i])[0] == nombre){
+                        var recetita = arrayRecetas[i];
+                        recetita.nombreReceta = nombreReceta2;
+                    }
+                }
+            }
+        }
+               
+        guardarLocalStorage();
+        window.location.reload();
     });
 
     $('#dataTableRecetas tbody').on('click', "#btnBorrar", function () {
@@ -61,6 +110,7 @@ $(document).ready(function(){
             guardarLocalStorage();
             alert("La receta ha sido borrada")
         }
+        window.location.reload();
     });
 });
 
@@ -96,7 +146,7 @@ const llenarTabla = () =>{
                 <td>${elemento.instrucciones}</td>
                 <td>
                     <button type="button" class="btn btn-info" id="btnInfo"><i class="bi bi-arrow-up-right-square-fill"></i></button>
-                    <button type="button" class="btn btn-warning" id="btnEditar"><i class="bi bi-pencil-fill"></i></button>
+                    <button class="btn btn-warning" id="btnEditar data-bs-toggle="modal" data-bs-target="#editarReceta""><i class="bi bi-pencil-fill"></i></button>
                     <button type="button" class="btn btn-danger" id="btnBorrar"><i class="bi bi-trash-fill"></i></button>
                 </td>
                 
@@ -124,5 +174,21 @@ formulario.addEventListener("submit", (e) =>{
     location.reload();
 
 })
+
+/* formulario2.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let nombreReceta = document.getElementById("nombreReceta").value;
+    let ingredientes = document.getElementById("ingredientes").value;
+    let instrucciones = document.getElementById("instrucciones").value;
+    let nombreReceta2 = document.getElementById("nombreReceta2").value;
+    let ingredientes2 = document.getElementById("ingredientes2").value;
+    let instrucciones2 = document.getElementById("instrucciones2").value;
+    crearReceta(nombreReceta, ingredientes, instrucciones);
+    guardarLocalStorage();
+
+    formulario2.reset();
+    location.reload();
+
+}) */
 
 document.addEventListener("DOMContentLoaded", llenarTabla);
